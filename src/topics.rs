185a6c6a74e471910c3d943fa7ec0aa5dabc6e44
Aswin Murali <aswin.murali2019@vitstudent.ac.in>
2022-09-30 22:33:10 +0530

    adds `EXPORT_POWER_TOPIC` & `EXPORT_BASE_PRICE_TOPIC` in `topics` file

---
+++ src/topics.rs
@@ -0,0 +1,5 @@
+/// MQTT topic with `export_power` value.
+pub(crate) const EXPORT_POWER_TOPIC: &str = "prosumers/+/export_power";
+
+/// MQTT topic with `export_base_price` value.
+pub(crate) const EXPORT_BASE_PRICE_TOPIC: &str = "prosumers/+/export_base_price";
