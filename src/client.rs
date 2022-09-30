ef5d22c41ececeb8d97bb4d14ca870ce2e072816
Aswin Murali <aswin.murali2019@vitstudent.ac.in>
2022-09-30 22:33:21 +0530

    formats code

--- src/client.rs
+++ src/client.rs
@@ -1,13 +1,9 @@
-use std::time::Duration;
-
+use colored::Colorize;
 use rumqttc::{AsyncClient, Event, EventLoop, Incoming, MqttOptions, QoS};
+use std::time::Duration;
 use uuid::Uuid;
 
-/// MQTT topic with `export_power` value.
-const EXPORT_POWER_TOPIC: &str = "prosumers/+/export_power";
-
-/// MQTT topic with `export_base_price` value.
-const EXPORT_BASE_PRICE_TOPIC: &str = "prosumers/+/export_base_price";
+use crate::topics::{EXPORT_BASE_PRICE_TOPIC, EXPORT_POWER_TOPIC};
 
 /// Get the MQTT client.
 pub(crate) fn get_client() -> (AsyncClient, EventLoop) {
@@ -20,8 +16,15 @@ pub(crate) fn get_client() -> (AsyncClient, EventLoop) {
     let port = dotenv::var("PORT").unwrap().parse::<u16>().unwrap();
 
     // Initialise MQTT options.
-    let mut options = MqttOptions::new(id, host, port);
+    let mut options = MqttOptions::new(&id, &host, port);
     options.set_keep_alive(Duration::from_secs(5));
+
+    info!(
+        "MQTT Client {}@{}:{}",
+        &id.on_white().black(),
+        &host.on_bright_white().black(),
+        format!("{}", &port).on_bright_yellow().black(),
+    );
 
     // Return MQTT client.
     AsyncClient::new(options, 10)
@@ -30,6 +33,17 @@ pub(crate) fn get_client() -> (AsyncClient, EventLoop) {
 /// Run the MQTT event loop and watch for messages.
 pub(crate) async fn run(client: AsyncClient, mut eventloop: EventLoop) {
     let qos = QoS::AtLeastOnce;
+
+    info!(
+        "{}",
+        format!(
+            "Listening topics {} & {}{}...",
+            &EXPORT_POWER_TOPIC.on_bright_purple().bright_yellow(),
+            &EXPORT_BASE_PRICE_TOPIC.on_bright_purple().bright_yellow(),
+            "".clear()
+        )
+        .bright_cyan()
+    );
 
     client.subscribe(EXPORT_POWER_TOPIC, qos).await.unwrap();
     client
@@ -41,7 +55,7 @@ pub(crate) async fn run(client: AsyncClient, mut eventloop: EventLoop) {
         let event = eventloop.poll().await;
 
         if let Ok(Event::Incoming(Incoming::Publish(publish))) = event {
-            println!("{:?}", publish.payload);
+            debug!("{:?}", publish.payload);
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
-/// Middleware
+use colored::Colorize;
+use std::env;
+
+#[macro_use]
+extern crate log;
 
 mod client;
+mod topics;
 
 #[tokio::main]
 async fn main() {
+    env::set_var("RUST_LOG", "info");
+    env_logger::init();
+
+    info!(
+        "{} {} version {}",
+        "Vaidyuti".bright_cyan(),
+        "Marketprice Middleware".yellow(),
+        env!("CARGO_PKG_VERSION")
+    );
+
+    info!("{}", "Reading .env...".bright_green());
     dotenv::from_filename(".env").ok();
 
     let (client, eventloop) = client::get_client();
