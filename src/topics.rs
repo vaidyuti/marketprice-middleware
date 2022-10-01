80879897859d7e77df21f058cfeda5e5c5d42329
Aswin Murali <aswin.murali2019@vitstudent.ac.in>
2022-10-01 17:45:19 +0530

    adds `PROSUMER_BASE_TOPIC` & `EXPORT_IS_EXTERNAL_TOPIC` & updates `EXPORT_BASE_PRICE_TOPIC` & `EXPORT_POWER_TOPIC`

--- src/topics.rs
+++ src/topics.rs
@@ -1,5 +1,13 @@
+use const_format::concatcp;
+
+/// MQTT topic of prosumers.
+pub const PROSUMER_BASE_TOPIC: &str = "prosumers/+";
+
 /// MQTT topic with `export_power` value.
-pub(crate) const EXPORT_POWER_TOPIC: &str = "prosumers/+/export_power";
+pub const EXPORT_POWER_TOPIC: &str = concatcp!(PROSUMER_BASE_TOPIC, "/export_power");
 
 /// MQTT topic with `export_base_price` value.
-pub(crate) const EXPORT_BASE_PRICE_TOPIC: &str = "prosumers/+/export_base_price";
+pub const EXPORT_BASE_PRICE_TOPIC: &str = concatcp!(PROSUMER_BASE_TOPIC, "/export_base_price");
+
+/// MQTT topic with `export_base_price` value.
+pub const EXPORT_IS_EXTERNAL_TOPIC: &str = concatcp!(PROSUMER_BASE_TOPIC, "/is_external");
